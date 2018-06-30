239a1803638ebc4932efdc8f8e2d8cf5706b0789
Damodar Shenoy <itsdamslife@gmail.com>
2018-06-30 22:22:46 +0530

    Removed unwanted imports and constructors from components

--- components/Greetings.js
+++ components/Greetings.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 class Greetings extends React.Component {
-  constructor() {
-    super()
-  }
   render() {
     return (
       <Text>Hello {this.props.name}!</Text>
@@ -13,9 +10,6 @@ class Greetings extends React.Component {
 }
 
 export default class LotsOfGreetings extends React.Component {
-  constructor() { 
-    super()
-  }
   render() {
     return (
       <View>
@@ -26,5 +20,3 @@ export default class LotsOfGreetings extends React.Component {
     );
   }
 }
-
-AppRegistry.registerComponent('HelloReact', () => LotsOfGreetings);
--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
-import { AppRegistry, View, Image } from 'react-native';
+import React from 'react';
+import { Image } from 'react-native';
 
 export default class Bananas extends React.Component {
-  constructor(){
-    super()
-  }
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <View>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-      </View>
+      <Image source={pic} style={{width: 193, height: 110}}/>
     );
   }
 }
-
-AppRegistry.registerComponent('HelloReact', () => Bananas);
